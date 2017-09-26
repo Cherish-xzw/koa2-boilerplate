770432eb8f1bdafb809f90bc94ab67922f61bc4f
travisxu <xuzhengwei.amor@gmail.com>
2017-09-26 20:53:06 +0800

    Refactor wepack build script

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,3 @@
-/* eslint no-console: 0 */
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -6,146 +5,148 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const AssetsWebpackPlugin = require('assets-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
-// const OpenBrowserPlugin = require('open-browser-webpack-plugin');
-// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const project = require('./project.config');
-const { chalkInfo } = require('./chalkConfig');
 
 const __DEV__ = project.globals.__DEV__;
 const __PROD__ = project.globals.__PROD__;
-// const __TEST__ = project.globals.__TEST__;
 
-console.log(
-  chalkInfo(
-    `============= [process.env.NODE_ENV = ${process.env
-      .NODE_ENV}] =============`
-  )
-);
-
-const webpackConfig = {
+const config = {
   context: project.paths.src('assets/javascripts'),
+
+  entry: {
+    application: './application.js',
+  },
+
+  output: {
+    path: project.paths.src('public'),
+    publicPath: __PROD__ ? project.production.compiler_public_path : '/',
+    filename: __DEV__ ? '[name].js' : 'js/[name].[chunkhash].js',
+    // work with lazy loading
+    chunkFilename: __DEV__ ? '[name].js' : 'js/[name].[chunkhash].js',
+  },
 
   resolve: {
     extensions: [ '.js', '.json' ],
   },
 
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          use: [{ loader: 'css-loader' }, { loader: 'sass-loader' }],
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|vendor\/assets)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [ 'es2015' ],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.woff(\?.*)?$/,
+        loader:
+          'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.woff2(\?.*)?$/,
+        loader:
+          'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2',
+      },
+      {
+        test: /\.otf(\?.*)?$/,
+        loader:
+          'file-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=font/opentype',
+      },
+      {
+        test: /\.ttf(\?.*)?$/,
+        loader:
+          'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream',
+      },
+      {
+        test: /\.eot(\?.*)?$/,
+        loader: 'file-loader?prefix=fonts/&name=[path][name].[ext]',
+      },
+      {
+        test: /\.svg(\?.*)?$/,
+        loader:
+          'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml',
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          name: '[name],[ext]',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: project.paths.src('assets/images'),
+        to: project.paths.src('public/images'),
+      },
+    ]),
+    new ExtractTextPlugin({
+      filename: __DEV__ ? 'application.css' : 'css/[name].[contenthash].css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      _: 'lodash',
+    }),
+    new webpack.DefinePlugin(project.globals),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendors',
+      minChunks: function(module) {
+        // any required modules inside node_modules are extracted to vendor
+        return (
+          module.resource &&
+          /\.js$/.test(module.resource) &&
+          module.resource.indexOf(path.join(__dirname, '../node_modules')) === 0
+        );
+      },
+    }),
+  ],
 };
 
-// ------------------------------------
-// Entry Points
-// ------------------------------------
-const APP_ENTRY = './application.js';
-
-webpackConfig.entry = {
-  application: APP_ENTRY,
-};
-
-// ------------------------------------
-// Bundle Output
-// ------------------------------------
-webpackConfig.output = {
-  path: project.paths.src('public'),
-  publicPath: __PROD__ ? project.production.compiler_public_path : '/',
-  filename: __DEV__ ? '[name].js' : 'js/[name].[chunkhash].js',
-  // work with lazy loading
-  chunkFilename: __DEV__ ? '[name].js' : 'js/[name].[chunkhash].js',
-};
-
-// ------------------------------------
-// Plugins List
-// ------------------------------------
-const copyImages = new CopyWebpackPlugin([
-  {
-    from: project.paths.src('assets/images'),
-    to: project.paths.src('public/images'),
-  },
-]);
-
-const extractSass = new ExtractTextPlugin({
-  filename: __DEV__ ? 'application.css' : 'css/[name].[contenthash].css',
-  // disable: __DEV__,
-});
-
-// fix legacy jQuery plugins which depend on globals
-const exposeGlobal = new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  _: 'lodash',
-});
-
-const occurrenceOrderPlugin = new webpack.optimize.OccurrenceOrderPlugin();
-const uglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({
-  compress: {
-    unused: true,
-    dead_code: true,
-    warnings: false,
-  },
-});
-
-const definePlugin = new webpack.DefinePlugin(project.globals);
-
-const commonsChunkPlugin = new webpack.optimize.CommonsChunkPlugin({
-  name: 'vendors',
-  minChunks: function(module) {
-    // any required modules inside node_modules are extracted to vendor
-    return (
-      module.resource &&
-      /\.js$/.test(module.resource) &&
-      module.resource.indexOf(path.join(__dirname, '../node_modules')) === 0
-    );
-  },
-});
-
-const assetsWebpackPlugin = new AssetsWebpackPlugin({
-  filename: 'assets_map.json',
-  path: project.paths.src('public'),
-  prettyPrint: true,
-});
-
-// ------------------------------------
-// Apply Plugins
-// ------------------------------------
-webpackConfig.plugins = [
-  copyImages,
-  extractSass,
-  exposeGlobal,
-  definePlugin,
-  // TODO: separation vendors.js && common.js
-  commonsChunkPlugin,
-];
-
-// ------------------------------------
-// Development Mode
-// ------------------------------------
 if (__DEV__) {
-  webpackConfig.devtool = project.development.compiler_devtool;
-  console.log(
-    chalkInfo(
-      '============= [Enabling plugins for live development (HMR, NoErrors)] ============= '
-    )
-  );
-  webpackConfig.plugins.push(
+  config.devtool = project.development.compiler_devtool;
+  config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
-    // new OpenBrowserPlugin({ url: project.development.compiler_public_path })
   );
 }
 
-// ------------------------------------
-// Production Mode
-// ------------------------------------
 if (__PROD__) {
-  webpackConfig.devtool = project.production.compiler_devtool;
-  console.log(
-    chalkInfo(
-      '============= [Enabling plugins for production (OccurenceOrder, UglifyJS, commonsChunk)] ============='
-    )
-  );
-  webpackConfig.plugins.push(
-    uglifyJsPlugin,
-    occurrenceOrderPlugin,
-    assetsWebpackPlugin,
+  config.devtool = project.production.compiler_devtool;
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        unused: true,
+        dead_code: true,
+        warnings: false,
+      },
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new AssetsWebpackPlugin({
+      filename: 'assets_map.json',
+      path: project.paths.src('public'),
+      prettyPrint: true,
+    }),
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
     new webpack.optimize.CommonsChunkPlugin({
@@ -166,79 +167,12 @@ if (__PROD__) {
         safe: true,
       },
     })
-    // TODO: shouldn't be here
-    // new BundleAnalyzerPlugin()
   );
 
   // https://webpack.js.org/configuration/performance
-  webpackConfig.performance = {
+  config.performance = {
     hints: 'warning',
   };
 }
 
-// ------------------------------------
-// Loaders
-// ------------------------------------
-webpackConfig.module.rules = [
-  {
-    test: /\.scss$/,
-    exclude: /node_modules/,
-    use: extractSass.extract({
-      use: [{ loader: 'css-loader' }, { loader: 'sass-loader' }],
-      fallback: 'style-loader',
-    }),
-  },
-  {
-    test: /\.js$/,
-    exclude: /(node_modules|vendor\/assets)/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: [ 'es2015' ],
-      },
-    },
-  },
-  {
-    test: /\.html$/,
-    loader: 'html-loader',
-  },
-  {
-    test: /\.woff(\?.*)?$/,
-    loader:
-      'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff',
-  },
-  {
-    test: /\.woff2(\?.*)?$/,
-    loader:
-      'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2',
-  },
-  {
-    test: /\.otf(\?.*)?$/,
-    loader:
-      'file-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=font/opentype',
-  },
-  {
-    test: /\.ttf(\?.*)?$/,
-    loader:
-      'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream',
-  },
-  {
-    test: /\.eot(\?.*)?$/,
-    loader: 'file-loader?prefix=fonts/&name=[path][name].[ext]',
-  },
-  {
-    test: /\.svg(\?.*)?$/,
-    loader:
-      'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml',
-  },
-  {
-    test: /\.(png|jpg)$/,
-    loader: 'url-loader',
-    options: {
-      limit: 8192,
-      name: '[name],[ext]',
-    },
-  },
-];
-
-module.exports = webpackConfig;
+module.exports = config;
