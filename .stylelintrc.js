a21fc82d4b809f6d4e0a9bbffc0a55ededacc8e7
travisxu <xuzhengwei.amor@gmail.com>
2017-11-10 17:38:39 +0800

    Add stylelint support

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,9 +3,10 @@
 module.exports = {
   // The standard config based on a handful of CSS style guides
   // https://github.com/stylelint/stylelint-config-standard
-  extends: 'stylelint-config-standard',
+  extends: 'stylelint-config-recommended-scss',
 
   plugins: [
+    'stylelint-scss',
     // stylelint plugin to sort CSS rules content with specified order
     // https://github.com/hudochenkov/stylelint-order
     'stylelint-order',
