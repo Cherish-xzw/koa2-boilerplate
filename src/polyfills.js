2d31162aa38650b2abc5a5b02f7e3a6cb3fb6342
travisxu <xuzhengwei.amor@gmail.com>
2018-02-03 22:58:52 +0800

    Add babel-polyfill in production

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,9 +1,17 @@
+// set default env to `development`
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
+// set taf env to `production`
 if (process.env.TAF_CONFIG) {
   process.env.NODE_ENV = "production";
 }
 
+// es6 API polyfill in production
+if (process.env.NODE_ENV === "production") {
+  require("babel-polyfill");
+}
+
+// use babel-register to compile koa middleware in development
 if (process.env.NODE_ENV === "development") {
   require("babel-register")({
     ignore: /node_modules\/(?!koa-*)/
